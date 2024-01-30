88e1809ed8668e9653e14898896c072859f328dc
jubayer44 <jubayerahmedshamim44@gmail.com>
2024-01-30 14:54:43 +0600

    added-404-pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,12 +4,14 @@ import { routesGenerator } from "../utils/routesGenerator";
 import { userPaths } from "./user.routes";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: routesGenerator(userPaths),
+    errorElement: <ErrorPage />,
   },
   {
     path: "/login",
